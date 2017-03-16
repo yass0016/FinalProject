0bbaab706d16d9a39c561db41877b359515d26bb
Tryton Van Meer <trytonvanmeer@gmail.com>
2017-03-15 22:45:40 -0400

    Added KitchenMainActivity and its layout file

---
+++ app/src/main/java/com/sayadev/finalproject/kitchen/KitchenMainActivity.java
@@ -0,0 +1,15 @@
+package com.sayadev.finalproject.kitchen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sayadev.finalproject.R;
+
+public class KitchenMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kitchen_main);
+    }
+}
