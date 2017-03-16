3915b0e0e9610a8313e4429b0e43858337edb0ad
Tryton Van Meer <trytonvanmeer@gmail.com>
2017-03-15 23:20:55 -0400

    Set second param for Intent constructor in onclick of appTwo button

--- app/src/main/java/com/sayadev/finalproject/MainActivity.java
+++ app/src/main/java/com/sayadev/finalproject/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.sayadev.finalproject.kitchen.KitchenMainActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button appOne;
@@ -33,8 +35,7 @@ public class MainActivity extends AppCompatActivity {
         appTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // replace null with our main application activity
-                Intent intent = new Intent(MainActivity.this, null);
+                Intent intent = new Intent(MainActivity.this, KitchenMainActivity.class);
                 startActivity(intent);
             }
         });
