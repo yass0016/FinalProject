8ba149e8253878646a2d8444d8e5a1c7ae2797fa
Tryton Van Meer <trytonvanmeer@gmail.com>
2017-03-15 23:29:49 -0400

    Added FloatingActionButton to the main kitchen activity

--- app/src/main/java/com/sayadev/finalproject/kitchen/KitchenMainActivity.java
+++ app/src/main/java/com/sayadev/finalproject/kitchen/KitchenMainActivity.java
@@ -1,5 +1,6 @@
 package com.sayadev.finalproject.kitchen;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +8,13 @@ import com.sayadev.finalproject.R;
 
 public class KitchenMainActivity extends AppCompatActivity {
 
+    protected FloatingActionButton addDeviceFAB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kitchen_main);
+
+        addDeviceFAB = (FloatingActionButton) findViewById(R.id.add_device_fab);
     }
 }
