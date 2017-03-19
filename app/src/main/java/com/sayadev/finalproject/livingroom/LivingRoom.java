c5b7d77d4ff4d4b5863418e44e6bc7f8c079e235
Saleh <yass0016@gmail.com>
2017-03-19 10:22:40 -0400

    added listview

--- app/src/main/java/com/sayadev/finalproject/livingroom/LivingRoom.java
+++ app/src/main/java/com/sayadev/finalproject/livingroom/LivingRoom.java
@@ -2,14 +2,21 @@ package com.sayadev.finalproject.livingroom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.sayadev.finalproject.R;
 
 public class LivingRoom extends AppCompatActivity {
 
+    private ListView roomItems;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_living_room);
+
+        roomItems = (ListView)findViewById(R.id.roomItems);
+
+
     }
 }
