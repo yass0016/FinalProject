27f56e977e223dcb2a378fab59c3ec365bcbac44
Saleh <yass0016@gmail.com>
2017-03-19 11:29:32 -0400

    Updated BaseActivity

--- app/src/main/java/com/sayadev/finalproject/BaseActivity.java
+++ app/src/main/java/com/sayadev/finalproject/BaseActivity.java
@@ -14,6 +14,7 @@ public class BaseActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         dbHelper = new ProjectDatabaseHelper(this);
     }
 
