7f4fc72b5ec71a8c0ec43d72494bfdfd387af49d
Tryton Van Meer <trytonvanmeer@gmail.com>
2017-03-20 02:53:43 -0400

    Added dialog for adding devices
    Dialog presents a Name and Model EditText field. A dropdown for selecting device type is to be implemented.

--- app/src/main/java/com/sayadev/finalproject/kitchen/KitchenMainActivity.java
+++ app/src/main/java/com/sayadev/finalproject/kitchen/KitchenMainActivity.java
@@ -1,22 +1,33 @@
 package com.sayadev.finalproject.kitchen;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.sayadev.finalproject.R;
 
 import java.util.ArrayList;
 
 public class KitchenMainActivity extends AppCompatActivity {
+
+    final Context context = this;
 
     protected FloatingActionButton addDeviceFAB;
     protected ListView deviceListView;
@@ -39,9 +50,50 @@ public class KitchenMainActivity extends AppCompatActivity {
         addDeviceFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Testing the addition of devices with fake data
-                deviceList.add(new KitchenDevice(KitchenDeviceType.MICROWAVE, "My Microwave", "SM17-80"));
-                deviceAdapter.notifyDataSetChanged();
+                LayoutInflater inflater =
+                        (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View dialogView = inflater.inflate(R.layout.dialog_kitchen_add_device, null);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle(R.string.kitchen_add_device_title)
+                        .setView(dialogView)
+                        .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        })
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                EditText nameEditText =
+                                        (EditText) dialogView.findViewById(R.id.add_kitchen_device_name);
+                                EditText modelEditText =
+                                        (EditText) dialogView.findViewById(R.id.add_kitchen_device_model);
+
+                                String name = nameEditText.getText().toString();
+                                String model = modelEditText.getText().toString();
+
+                                if (name.matches("") || model.matches(""))
+                                {
+                                    Toast.makeText(context,
+                                            R.string.kitchen_add_device_empty_field,
+                                            Toast.LENGTH_SHORT).show();
+
+                                    return;
+                                }
+
+                                KitchenDevice device = new KitchenDevice(
+                                        KitchenDeviceType.MICROWAVE,
+                                        name, model);
+
+                                deviceList.add(device);
+                                deviceAdapter.notifyDataSetChanged();
+                            }
+                        });
+
+                AlertDialog addDeviceDialog = builder.create();
+                addDeviceDialog.show();
             }
         });
     }
