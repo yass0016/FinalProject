292198faf2c135b149cf5219bf6e536a106ec7ca
EvoPheonixSchool <mcgr0235@algonquinlive.com>
2017-03-27 19:54:17 -0400

    This is the initial commit for the automobile.
    added activity
    added listview for items
    pressing on item opens dialog box with more info
    added 2 edit texts for name and description of item
    added button for adding items to list

--- app/src/main/java/com/sayadev/finalproject/MainActivity.java
+++ app/src/main/java/com/sayadev/finalproject/MainActivity.java
@@ -1,11 +1,12 @@
 package com.sayadev.finalproject;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.sayadev.finalproject.automobile.automobile;
 import com.sayadev.finalproject.kitchen.KitchenMainActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -55,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // replace null with our main application activity
-                Intent intent = new Intent(MainActivity.this, null);
+                Intent intent = new Intent(MainActivity.this, automobile.class);
                 startActivity(intent);
             }
         });
