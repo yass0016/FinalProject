2fafab949685385ffe1102b928a2cd23714af75d
saleh <yass0016@gmail.com>
2017-03-30 15:58:47 -0400

    updated main activity

--- app/src/main/java/com/sayadev/finalproject/livingroom/RoomData.java
+++ app/src/main/java/com/sayadev/finalproject/livingroom/RoomData.java
@@ -1,24 +1,50 @@
 package com.sayadev.finalproject.livingroom;
 
+import android.util.Log;
+
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /**
  * Created by saleh on 2017-03-25.
  */
 
 public class RoomData {
+    public enum DeviceType {
+        TV,
+        LAMP,
+        BLINDING
+    }
+
     private int _id;
     private String title;
     private String imageUri;
+    private int itemType;
+    private String itemName;
+    private Date lastVisitDate;
+    private Date createdDate;
 
     public RoomData() {
         this._id = 0;
         this.title = "";
         this.imageUri = "";
+
+        this.itemType = DeviceType.TV.ordinal();
+        itemName = "";
+        lastVisitDate = new Date();
+        createdDate = new Date();
     }
 
-    public RoomData(int _id, String title, String imageUri) {
+    public RoomData(int _id, String title, String imageUri, int itemType, String itemName, Date lastVisitDate, Date createdDate) {
         this._id = _id;
         this.title = title;
         this.imageUri = imageUri;
+        this.itemType = itemType;
+        this.itemName = itemName;
+        this.lastVisitDate = lastVisitDate;
+        this.createdDate = createdDate;
     }
 
     public int get_id() {
@@ -43,5 +69,37 @@ public class RoomData {
 
     public void setImageUri(String imageUri) {
         this.imageUri = imageUri;
+    }
+
+    public int getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(int itemType) {
+        this.itemType = itemType;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Date getLastVisitDate() {
+        return lastVisitDate;
+    }
+
+    public void setLastVisitDate(Date lastVisitDate) {
+        this.lastVisitDate = lastVisitDate;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
     }
 }
